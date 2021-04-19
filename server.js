c5076b9e492274f0aeb0abdedfede57ed2e28ca0
macizomedia <info@macizomedia.com>
2021-04-19 16:00:29 +0200

    minimal server running -no express

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const http = require('http')
 const url = require('url')
 const fs = require('fs')
 const path = require('path')
+var mimes = require('./mimeType.json')
 
 const mimeType = {
     '.ico': 'image/x-icon',
@@ -24,18 +25,25 @@ const mimeType = {
 var basePath = './'
 
 const server = http.createServer((req, res) => {
+    /* var extension = req.url.substring(req.url.lastIndexOf('.'))
+
+    var type = mimes[extension]
+    if (type) {
+        req.writeHead('content-type', type)
+    } */
     const headers = {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
         'Access-Control-Max-Age': 2592000,
         'Access-Control-Allow-Headers': '*',
+        'content-type': 'text/html',
     }
     var resolvedBase = path.resolve(basePath)
     var safeSuffix = path.normalize(req.url).replace(/^(\.\.[\/\\])+/, '')
     var fileLoc = path.join(resolvedBase, safeSuffix)
 
     if (req.url === '/') {
-        res.writeHead(200, { 'content-type': 'text/html' })
+        res.writeHead(200, headers)
         fs.createReadStream('home.html').pipe(res)
     } else if (req.url.match('.css$')) {
         res.writeHead(200, { 'Content-Type': 'text/css' })
@@ -46,6 +54,16 @@ const server = http.createServer((req, res) => {
     } else if (req.url.match('.js$')) {
         res.writeHead(200, { 'Content-Type': 'application/json' })
         fs.createReadStream(fileLoc, 'UTF-8').pipe(res)
+    } else {
+        fs.readFile(__dirname + req.url, function (err, data) {
+            if (err) {
+                res.writeHead(404)
+                res.end(JSON.stringify(err))
+                return
+            }
+            res.writeHead(200)
+            res.end(data)
+        })
     }
 })
 
